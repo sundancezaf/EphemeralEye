e3b527c974133f6ac7d105b46fb4fa1a7e515d7c
sundancezaf <zaleman@uiowa.edu>
2022-03-31 01:28:47 -0700

    third

--- main.py
+++ main.py
@@ -63,22 +63,37 @@ class main:
 class no_extension_cleanup():
     def __init__(self, filetoRead):
         self.filetoRead = filetoRead
-        self.pii_values = {"Amex":"^3[47][0-9]{13}$",
-    "BCGlobal":"^(6541|6556)[0-9]{12}$",
-    "Carte Blanche":"^389[0-9]{11}$",
-    "Diners Club":"^3(?:0[0-5]|[68][0-9])[0-9]{11}$",
-    "Discover":"^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})$",
-    "Insta Payment":"^63[7-9][0-9]{13}$",
-    "JCB":"^(?:2131|1800|35\d{3})\d{11}$",
-    "KoreanLocal":"^9[0-9]{15}$",
-    "Laser":"^(6304|6706|6709|6771)[0-9]{12,15}$",
-    "Maestro":"^(5018|5020|5038|6304|6759|6761|6763)[0-9]{8,15}$",
-    "Master":"^(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))$",
-    "Solo":"^(6334|6767)[0-9]{12}|(6334|6767)[0-9]{14}|(6334|6767)[0-9]{15}$",
-    "Switch":"^(4903|4905|4911|4936|6333|6759)[0-9]{12}|(4903|4905|4911|4936|6333|6759)[0-9]{14}|(4903|4905|4911|4936|6333|6759)[0-9]{15}|564182[0-9]{10}|564182[0-9]{12}|564182[0-9]{13}|633110[0-9]{10}|633110[0-9]{12}|633110[0-9]{13}$",
-    "Union Pay":"^(62[0-9]{14,17})$",
-    "Visa":"^4[0-9]{12}(?:[0-9]{3})?$",
-    "Social Security Number":"^(?!000|666)[0-8][0-9]{2}-(?!00)[0-9]{2}-(?!0000)[0-9]{4}$"}
+
+    def line_cleanup(self, aString):
+        pii_values = {"Amex":"^3[47][0-9]{13}$",
+        "BCGlobal":"^(6541|6556)[0-9]{12}$",
+        "Carte Blanche":"^389[0-9]{11}$",
+        "Diners Club":"^3(?:0[0-5]|[68][0-9])[0-9]{11}$",
+        "Discover":"^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})$",
+        "Insta Payment":"^63[7-9][0-9]{13}$",
+        "JCB":"^(?:2131|1800|35\d{3})\d{11}$",
+        "KoreanLocal":"^9[0-9]{15}$",
+        "Laser":"^(6304|6706|6709|6771)[0-9]{12,15}$",
+        "Maestro":"^(5018|5020|5038|6304|6759|6761|6763)[0-9]{8,15}$",
+        "Master":"^(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))$",
+        "Solo":"^(6334|6767)[0-9]{12}|(6334|6767)[0-9]{14}|(6334|6767)[0-9]{15}$",
+        "Switch":"^(4903|4905|4911|4936|6333|6759)[0-9]{12}|(4903|4905|4911|4936|6333|6759)[0-9]{14}|(4903|4905|4911|4936|6333|6759)[0-9]{15}|564182[0-9]{10}|564182[0-9]{12}|564182[0-9]{13}|633110[0-9]{10}|633110[0-9]{12}|633110[0-9]{13}$",
+        "Union Pay":"^(62[0-9]{14,17})$",
+        "Visa":"^4[0-9]{12}(?:[0-9]{3})?$",
+        "Social Security Number":"^(?!000|666)[0-8][0-9]{2}-(?!00)[0-9]{2}-(?!0000)[0-9]{4}$"}
+        aStringList = aString.split()
+        for section in aStringList:
+            # Check if it's all alpha chars again
+            check1 = str(section).isalpha()
+            if not check1:
+                # Here is where you check if it matches the parameters
+                for key,value in pii_values.items():
+                    value_search = re.search(str(value),section)
+                    if value_search:
+                        return key, section
+                        #print(key, section)
+            return 0
+
 
     def cleanup(self):
         #print("Yo")clear
@@ -91,23 +106,18 @@ class no_extension_cleanup():
             linecount = 0
             line = file.readline()
             while line:
-                linecount +=1
-                lineList = line.split(",")
-                for section in lineList:
-                    section = str(section)
-                    for key,value in self.pii_values.items():
-                        if (key=="Social Security Number"):
-                            value_search = re.search(str(value),section)
-                        else:
-                            #section_stripped = section.strip("-")
-                            section_stripped = section.strip(",")
-                            section_stripped = section_stripped.strip()
-                            value_search = re.search(str(value), section_stripped)
-
-                        if value_search:
-                            occurrences.write(f"File name: {self.filetoRead}, Line Number: {linecount}, Type: {key} \n")                 
+                linecount += 1
+                line = line.strip()
+                isalpha = line.isalpha()
+                if not isalpha:
+                    # Check here
+                    check3 = self.line_cleanup(line)
+                    if check3 != 0:
+                        occurrences.write(f"File name: {self.filetoRead} Line Number: {linecount} Value: {check3}\n")
                 line = file.readline()
-            
+                    
+                          
+    
 
 
 class csv_files_cleanup():
